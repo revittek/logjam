b3ca142e75a200afb97c96d9b0d68f0fbb077586
Kate Revitte <kate-rev@kates-air.attlocal.net>
2019-01-05 20:30:35 -0600

    Autocomplete chip categories

--- src/app/submit/submit.component.ts
+++ src/app/submit/submit.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { Phrase } from '../phrase.model';
 import { AngularFirestore } from 'angularfire2/firestore';
-import { NgForm } from '@angular/forms';
-import { MatSnackBar, MatSnackBarModule } from '@angular/material';
-import { FormGroup, FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NgForm, FormGroup, FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatSnackBar, MatSnackBarModule, MatAutocompleteSelectedEvent, MatChipInputEvent, MatAutocomplete } from '@angular/material';
+import {COMMA, ENTER} from '@angular/cdk/keycodes';
+import {Observable} from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
 
 @Component({
   selector: 'app-submit',
@@ -11,11 +13,26 @@ import { FormGroup, FormControl, FormsModule, ReactiveFormsModule, Validators } 
   styleUrls: ['./submit.component.css']
 })
 export class SubmitComponent implements OnInit {
+  visible = true;
+  selectable = true;
+  removable = true;
+  addOnBlur = true;
+  separatorKeysCodes: number[] = [ENTER, COMMA];
+  categoryCtrl = new FormControl();
+  filteredCategories: Observable<string[]>;
+  categories: string[] = ['2019'];
+  allCategories: string[] = [];
+
+  @ViewChild('categoryInput') categoryInput: ElementRef<HTMLInputElement>;
+  @ViewChild('auto') matAutocomplete: MatAutocomplete;
+
   submitForm: FormGroup;
   snackBar: MatSnackBar;
-  public categories: Array<string> = [];
   constructor(private db: AngularFirestore, public snickers: MatSnackBar) {
     this.snackBar = snickers;
+    this.filteredCategories = this.categoryCtrl.valueChanges.pipe(
+        startWith(null),
+        map((category: string | null) => category ? this._filter(category) : this.allCategories.slice()));
   }
 
   ngOnInit() {
@@ -23,7 +40,7 @@ export class SubmitComponent implements OnInit {
   	this.submitForm = new FormGroup({
       phrase: new FormControl('', { validators: [Validators.required] }),
       user: new FormControl('', { validators: [Validators.required] }),
-      categories: new FormControl(null, { validators: [] })
+      allCategories: new FormControl(null, { validators: [] })
     });
   }
 
@@ -55,20 +72,20 @@ export class SubmitComponent implements OnInit {
   }
 
   private retrieveCategories() {
-    this.categories = [];
+    this.allCategories = [];
     let result = this.db.collection('categories').get();
     result.forEach(value =>
       {
         value.forEach(entry =>
         {
-          this.categories.push(entry.get("categoryName"))
+          this.allCategories.push(entry.get("categoryName"))
       });
     });
   }
 
     /** Save the node to database */
   saveItem(newCategory: string) {
-    if(!this.categories.includes(newCategory)){
+    if(!this.allCategories.includes(newCategory)){
       let addCategory = {
         'categoryName': newCategory
       }
@@ -80,5 +97,44 @@ export class SubmitComponent implements OnInit {
       });
     }
   }
+  add(event: MatChipInputEvent): void {
+    // Add fruit only when MatAutocomplete is not open
+    // To make sure this does not conflict with OptionSelected Event
+    if (!this.matAutocomplete.isOpen) {
+      const input = event.input;
+      const value = event.value;
 
+      // Add our fruit
+      if ((value || '').trim()) {
+        this.categories.push(value.trim());
+      }
+
+      // Reset the input value
+      if (input) {
+        input.value = '';
+      }
+
+      this.categoryCtrl.setValue(null);
+    }
+  }
+
+  remove(category: string): void {
+    const index = this.categories.indexOf(category);
+
+    if (index >= 0) {
+      this.categories.splice(index, 1);
+    }
+  }
+
+  selected(event: MatAutocompleteSelectedEvent): void {
+    this.categories.push(event.option.viewValue);
+    this.categoryInput.nativeElement.value = '';
+    this.categoryCtrl.setValue(null);
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.allCategories.filter(category => category.toLowerCase().indexOf(filterValue) === 0);
+  }
 }
